91ecba3be529f0cc94e6c8c03134e7b909865b9d
wael <wael.slama.issat@gmail.com>
2025-02-05 11:03:43 +0100

    Setup the project

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,14 @@
+/* eslint-disable prettier/prettier */
+
+import { Injectable } from "@nestjs/common";
+
+
+@Injectable({})
+export class AuthService{
+    signin(){
+        return {msg:'I am logged in'}
+    }
+    signup(){
+        return {msg:'I am signed up'}
+    }
+}
