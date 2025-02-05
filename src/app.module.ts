c2acbaff08001fe800546f2b18a1928e1281ad0d
wael <wael.slama.issat@gmail.com>
2025-02-05 16:37:24 +0100

    signUp and signIn

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,17 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
-
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule,UserModule, BookmarkModule],
-
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal:true,
+    }),
+    AuthModule, 
+    UserModule, 
+    BookmarkModule, 
+    PrismaModule],
 })
 export class AppModule {}
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,17 +1,19 @@
 /* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-    constructor(){
-        super({
-            datasources:{
-                db:{
-                    url:'postgresql://postgres:123@localhost:5434/nest?schema=public'
-                }
-            }
-        })
-
-    }
+  constructor(config : ConfigService) {
+    super({
+      datasources: {
+        db: {
+          url: config.get('DATABASE_URL'),
+        },
+      },
+    });
+    console.log(config.get('DATABASE_URL'));
+    
+  }
 }
